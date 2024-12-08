fb7ed89b4d9c0bae28647a5d8c27b6ce29123f5b
Orkea <orkea@hotmail.fr>
2024-12-08 22:23:42 +0100

    Ajout de Sass

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom"
 const Header = () => {
-  return <>
-  <div>Le header</div>
-  <Link to="/">Accueil</Link>
-  <Link to='a_propos'> A Propos</Link>
-  </>
-  
+  return (
+    <>
+      <div className="header-container">Le header</div>
+      <Link to="/">Accueil</Link>
+      <Link to="a_propos"> A Propos</Link>
+    </>
+  )
 }
 export default Header
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import "./styles/index.scss"
+
 /** Importation des différentes routes */
 import Home from "./pages/Home.jsx"
 import A_Propos from "./pages/A_Propos.jsx"
 import Fiche_logement from "./pages/Fiche_logement.jsx"
 import Error from './pages/Error.jsx'
 
-/** Importation des composants */
+/** Importation des diférents composants */
 import Header from "./components/Header.jsx"
 import Footer from "./components/Footer.jsx"
 
