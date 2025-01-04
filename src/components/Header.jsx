482ea7fc11b85ef8ee2d3da249eaa597518f0476
Orkea <orkea@hotmail.fr>
2025-01-04 14:26:04 +0100

    Maj du header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,12 @@ const Header = () => {
   return (
     <div className="header">
       <nav className="header__navbar">
+        <div className="header__accueil">
         <Link to="/" className="header__link">Accueil</Link>
+        </div>
+        <div className="header__a_propos"> 
         <Link to="a_propos" className="header__link"> A Propos</Link>
+        </div>
       </nav>
       <img src={logo} alt="logo" className="header__logo"/>
     </div>
