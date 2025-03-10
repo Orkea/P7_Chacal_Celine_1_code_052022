2551c94974135b60b686026fb8f5f7753ec32f14
Orkea <orkea@hotmail.fr>
2025-03-10 08:31:41 +0100

    Maj du css et du House .jsx

--- src/components/House.jsx
+++ src/components/House.jsx
@@ -20,31 +20,36 @@ const House = ({
   return (
     <main className="house">
       <Carrousel pictures={pictures} title={title} />
-      <div className="house__title">
-        <h2>{title}</h2>
-        <h3>{location}</h3>
-      </div>
-     
       <div className="house-content">
-        <div className="house-content__tags">
-          {tags.map((tag, index) => (
-            <div key={`${tag}-${index}`} className="house-content__tags--item">
-              <span>{tag}</span>
+        <div className="house-content__title-aside">
+          <div className="house-content__title-tags">
+            <div className="house-content__title">
+              <h2>{title}</h2>
+              <h3>{location}</h3>
             </div>
-          ))}
+            <div className="house-content__tags">
+              {tags.map((tag, index) => (
+                <div
+                  key={`${tag}-${index}`}
+                  className="house-content__tags--item"
+                >
+                  <span>{tag}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+          <aside className="house-aside">
+            <div className="house-aside__host">
+              <p>
+                {firstName} <br />
+                {lastName}
+              </p>
+              <img src={host.picture} alt={host.name} />
+            </div>
+              <HouseRating rating={rating} />
+            
+          </aside>
         </div>
-        <aside className="house-aside">
-          <div className="house-aside__host">
-            <p>
-              {firstName} <br />
-              {lastName}
-            </p>
-            <img src={host.picture} alt={host.name} />
-          </div>
-          <div className="house-rating">
-            <HouseRating rating={rating} />
-          </div>
-        </aside>
         <div className="house-content__collapse">
           <Collapse
             element={description}
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,10 @@
-import Logo404 from "../assets/404.svg"
+// import Logo404 from "../assets/404.svg"
 import { Link } from "react-router-dom"
 const Error = () => {
   return (
     <div className="error">
-      <img src={Logo404} alt="Erreur 404" />
+      {/* <img src={Logo404} alt="Erreur 404" /> */}
+      <p className="error__404">404</p>
       <p className="error__text">Oups! La page que vous demandez n&apos;existe pas.</p>
       <Link to="/" className="error__link">Retourner sur la page d’accueil</Link>
     </div>
